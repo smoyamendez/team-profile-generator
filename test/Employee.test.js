5ec7cc37ff227ea70a2139b04b4f5aad29c78157
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-03-09 23:04:30 -0500

    first employee test & code  written & passing

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,13 +1,15 @@
 // Starter file provided by Instructor (03/09/2021) AC
 
+const { it, expect } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
-
+    const employee = new Employee();
+    expect(typeof(employee)).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
-
+    
 });
 
 test("Can set id via constructor argument", () => {
@@ -23,7 +25,7 @@ test("Can get name via getName()", () => {
 });
 
 test("Can get id via getId()", () => {
-
+    // return getId().then()
 });
 
 test("Can get email via getEmail()", () => {
