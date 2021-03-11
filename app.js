0cbe0dd6f57b31151c44b0eb5f753d1b327338fb
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-03-10 20:17:41 -0500

    functions to initilize inquirer prompts written

--- app.js
+++ app.js
@@ -12,17 +12,11 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// array to push new employees to
+let employees = [];
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
@@ -34,38 +28,31 @@ const render = require("./lib/htmlRenderer");
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
 
-
-
-let employees = [];
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 const managerQuestions = [
     {
         type: 'input',
         message: 'What is the team manager\'s name?',
         name: 'name'
     },
-
+    
     {
         type: 'input',
         message: 'What is the team manager\'s id?',
         name: 'id'
     },
-
+    
     {
         type: 'input',
         message: 'What is the team manager\'s email?',
         name: 'email'
     },
-
+    
     {
         type: 'input',
         message: 'What is the team manager\'s office number?',
         name: 'officeNum'
     },
-
+    
     {
         type: 'list',
         message: 'Which type of team member would you like to add?',
@@ -79,25 +66,25 @@ const engineerQuestions = [
         message: 'What is the team engineer\'s name?',
         name: 'name'
     },
-
+    
     {
         type: 'input',
         message: 'What is the team engineer\'s id?',
         name: 'id'
     },
-
+    
     {
         type: 'input',
         message: 'What is the team engineer\'s email?',
         name: 'email'
     },
-
+    
     {
         type: 'input',
         message: 'What is the team engineer\'s GitHub username?',
         name: 'github'
     },
-
+    
     {
         type: 'list',
         message: 'Which type of team member would you like to add?',
@@ -111,25 +98,25 @@ const internQuestions = [
         message: 'What is the team intern\'s name?',
         name: 'name'
     },
-
+    
     {
         type: 'input',
         message: 'What is the team intern\'s id?',
         name: 'd'
     },
-
+    
     {
         type: 'input',
         message: 'What is the team intern\'s email?',
         name: 'email'
     },
-
+    
     {
         type: 'input',
         message: 'What is the team intern\'s school?',
         name: 'school'
     },
-
+    
     {
         type: 'list',
         message: 'Which type of team member would you like to add?',
@@ -139,18 +126,50 @@ const internQuestions = [
 ]
 
 
-function init() {
+function initAll() {
     inquirer.prompt(managerQuestions)
-
-        .then((answers) => {
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNum)
-            employees.push(manager);
-
-            if(answers.role === "Engineer") {
-
-            }
-
-        })
+    
+    .then((answers) => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNum);
+        employees.push(manager);
+        
+        if(answers.role === "Engineer") {
+            engineerDetails();
+        } else if (answers.role === "Intern") {
+            internDetails();
+        } else {
+            console.log('Thanks for adding your team members');
+            return;
+        }
+        
+    })
 }
 
-let managerDetails
+function internDetails () {
+    inquirer.prompt(internQuestions)
+    .then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        employees.push(intern);
+    })
+}
+function engineerDetails () {
+    inquirer.prompt(engineerQuestions)
+    .then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        employees.push(engineer);
+    })
+}
+
+initAll();
+
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
